4603fce3c8a10f23e1b4cb12f2a542af3a7e295f
MarioBassem <mariobassem12@gmail.com>
2017-10-12 18:16:43 +0200

    Dynamic Programming

---
+++ UVA 10911 - Forming Quiz Teams.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define inf 1000000000
+#define vi vector<int>
+#define vii vector< vector<int> >
+
+int n;
+double memo[(1<<16)];
+vii in;
+
+double dp(int u)
+{
+    if(u == (1<<2*n) - 1) return 0.0;
+    double &ret = memo[u];
+    if(ret != -1.0) return ret;
+    ret = inf;
+    int temp;
+    for(int i=0; i<2 * n; i++){
+        if((1<<i) & u) continue;
+        for(int j = i + 1; j<2 * n; j++){
+            if((1<<j) & u) continue;
+            temp = u | (1<<i) | (1<<j);
+            ret = min(ret, dp(temp) + hypot(in[i][0] - in[j][0], in[i][1] - in[j][1]) );
+        }
+    }
+    return ret;
+}
+
+int main()
+{
+    int tc = 1;
+    char s[25];
+    while(1){
+        scanf("%d", &n);
+        if(!n) return 0;
+        int x, y;
+        in.assign(2 * n, vi(2) );
+        for(int i=0; i<(1<<(2*n)); i++) memo[i] = -1.0;
+        for(int i=0; i<2 * n; i++){
+            scanf("%s%d%d", &s, &in[i][0], &in[i][1]);
+        }
+        printf("Case %d: %.2lf\n", tc++, dp(0) );
+    }
+}
